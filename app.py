b6ffe2af88ff9164a2a8b53438070ccc0850f95d
Siree16 <149429165+Siree16@users.noreply.github.com>
2023-10-31 01:04:02 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+def hello_world():
+    return "Hello, there!"
+
+@app.route('/welcome')
+
+def hello_me():
+    return "Hi, welcome to Task2 of SDE Assgn"
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=80)
